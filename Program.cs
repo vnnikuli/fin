7864a2fa246218de2c03d836e965c146bf459486
Vadim <yrl.nikulin.v@gmail>
2023-03-05 13:38:33 +0300

    пределение нового массива (4)

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+string[] ModArray(string[] arr)
+{
+    int k = 0;  
+    for (int i = 0; i < arr.Length; i++)    //определение длины нового массива
+    {
+        if (arr[i].Length <= 3)   //определение длины нового массива
+        {
+            k++;
+        }
+    }
+}
